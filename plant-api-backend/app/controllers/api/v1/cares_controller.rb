da3475ba70cbcecb1c3578ff0695a81e84fdb555
github username <github email address>
2020-07-07 15:59:05 -0500

    ran migration

--- plant-api-backend/app/controllers/api/v1/cares_controller.rb
+++ plant-api-backend/app/controllers/api/v1/cares_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::CaresController < ApplicationController
+    def create 
+        @care = Care.create(check_params)
+        if @care.valid?
+            render json: @care 
+        else 
+            render json: {errors: @care.errors.full_messages}
+        end 
+    end
+    private
+    def check_params
+        permit(:maintenance, :light, :plant_id)
+    end 
 end
--- plant-api-backend/app/controllers/api/v1/plants_controller.rb
+++ plant-api-backend/app/controllers/api/v1/plants_controller.rb
@@ -1,2 +1,23 @@
 class Api::V1::PlantsController < ApplicationController
+    def index
+        @plants = Plant.all
+        render json: @plants, status 200
+    end 
+    def show 
+        @plant = Plant.find(params[:id])
+        render json: @plant
+    end
+    def update 
+        @plant = Plant.find(params[:id]);
+        @plant.update(check_params)
+        if @plant.valid?
+            render json: @plant
+        else 
+            render json: {errors: @plant.errors.full_messages}
+        end 
+    end 
+    private 
+    def checks_params
+        params.permit(:name, :description)
+    end 
 end
